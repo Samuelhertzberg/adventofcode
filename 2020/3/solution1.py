ab44c3701885ec5ad1e4b5639ca40040ea51593f
Samuel-hertzberg <Samuel-hertzberg>
2020-12-03 23:45:51 +0100

    day 3

--- 2020/3/solution1.py
+++ 2020/3/solution1.py
@@ -19,11 +19,6 @@ def short1():
 def short2(vec):
     return len([l for i, l in enumerate(open('input').readlines()) if i % vec[1] == 0 and l[int(i*vec[0]/vec[1]) % (len(l) - 1) ] == '#'])
 
-def arvids():
-    print(sum([(l[i*3 % len(l)] == '#') for i, l in enumerate(open('input').read().splitlines())]))
-
-        
-
 if __name__ == '__main__':
     print(solution([1,1])*solution([3,1])*solution([5,1])*solution([7,1])*solution([1,2]))
     print(solution([3,1]))
