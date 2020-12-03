1f6b26289aa26bca6b93a15465e7a321e99e83f3
Samuel-hertzberg <Samuel-hertzberg>
2020-12-03 23:44:22 +0100

    ONELINERS AAAAAAAAA

--- 2020/3/solution1.py
+++ 2020/3/solution1.py
@@ -13,8 +13,23 @@ def solution(vec):
             pos += vec[0] 
         return trees
 
+def short1():
+    return len([l for i, l in enumerate(open('input').readlines()) if l[(i*3) % (len(l) - 1) ] == '#'])
+
+def short2(vec):
+    return len([l for i, l in enumerate(open('input').readlines()) if i % vec[1] == 0 and l[int(i*vec[0]/vec[1]) % (len(l) - 1) ] == '#'])
+
+def arvids():
+    print(sum([(l[i*3 % len(l)] == '#') for i, l in enumerate(open('input').read().splitlines())]))
+
+        
+
 if __name__ == '__main__':
-    print(solution([3,1]))
     print(solution([1,1])*solution([3,1])*solution([5,1])*solution([7,1])*solution([1,2]))
+    print(solution([3,1]))
+    print(short1())
+
+    print(solution([3,2]))
+    print(short2([3,2]))
     
     
